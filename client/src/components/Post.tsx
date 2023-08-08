f4c0aca00cc822a450cf8546e06668ebeb0d9b95
Umair Syed <itsumairsyed@gmail.com>
2023-08-08 10:34:41 +0500

    Link wrapper removed from post

--- client/src/components/Post.tsx
+++ client/src/components/Post.tsx
@@ -1,10 +1,4 @@
-import {
-  Grid,
-  IconButton,
-  Typography,
-  Menu,
-  MenuItem,
-} from "@mui/material";
+import { Grid, IconButton, Typography, Menu, MenuItem } from "@mui/material";
 import { Box } from "@mui/system";
 import formatDistanceToNow from "date-fns/formatDistanceToNow";
 import React, { useEffect, useState, useContext } from "react";
@@ -19,7 +13,7 @@ import { useMutation, useQueryClient } from "@tanstack/react-query";
 import { deletePostApi, likePostApi } from "../services/api/Post";
 
 export default function Post({ post, profile }) {
-  const [isLiked, setIsLiked] = useState(false)
+  const [isLiked, setIsLiked] = useState(false);
 
   const queryClient = useQueryClient();
 
@@ -41,7 +35,7 @@ export default function Post({ post, profile }) {
     },
     onSuccess: (data) => {
       console.log(data);
-      setIsLiked(true)
+      setIsLiked(true);
       queryClient.invalidateQueries("profile");
     },
   });
@@ -61,7 +55,7 @@ export default function Post({ post, profile }) {
 
   const handleLike = async (e) => {
     e.preventDefault();
-    likeMutate({ userId: user._id, token, postId: post?._id })
+    likeMutate({ userId: user._id, token, postId: post?._id });
   };
 
   const handleDeletePost = async (e) => {
@@ -72,121 +66,117 @@ export default function Post({ post, profile }) {
   };
 
   useEffect(() => {
-    if(post.likes.includes(user?._id)) {
-      setIsLiked(true)
+    if (post.likes.includes(user?._id)) {
+      setIsLiked(true);
     }
-  }, [])
-  
+  }, []);
+
   return (
     <>
-      <Link
-        to={`/posts/${post._id}`}
-        style={{ textDecoration: "none", color: "inherit" }}
+      <Box
+        padding="1rem"
+        sx={{
+          "&:hover": {
+            backgroundColor: "#eee",
+          },
+        }}
       >
-        <Box
-          padding="1rem"
-          sx={{
-            "&:hover": {
-              backgroundColor: "#eee",
-            },
-          }}
-        >
-          <Grid container flexWrap="nowrap">
-            <Grid item sx={{ paddingRight: "1rem" }}>
-              <Link to={`/profile/${post._id}`}>
-                <img src={profile.profileUrl} alt="lgoog" width="50px" />
-              </Link>
-            </Grid>
-            <Grid item flexGrow="1">
-              <Box>
-                <Grid
-                  container
-                  justifyContent="space-between"
-                  alignItems="center"
-                  flexWrap="nowrap"
-                >
-                  <Grid item>
-                    <Box display="flex">
-                      <Typography
-                        sx={{ fontSize: "16px", fontWeight: 500, mr: "6px" }}
-                      >
-                        {profile.name}
-                      </Typography>
-                      <Typography
-                        sx={{ fontSize: "15px", mr: "6px", color: "#555" }}
-                      >
-                        @{profile.email}
-                      </Typography>
-                      <Typography
-                        sx={{ fontSize: "15px", mr: "6px", color: "#555" }}
-                      >
-                        .
-                      </Typography>
-                      <Typography
-                        sx={{ fontSize: "15px", mr: "6px", color: "#555" }}
-                      >
-                        {formatDistanceToNow(new Date(post.createdAt))}
-                      </Typography>
-                    </Box>
-                    <Box>
-                      <Typography sx={{ fontSize: "15px", color: "#555" }}>
-                        {post.text}
-                      </Typography>
-                    </Box>
-                  </Grid>
-                  <Grid item>
-                    {profile?._id === user?._id && (
-                      <IconButton
-                        aria-expanded={open ? "true" : undefined}
-                        onClick={(e) => {
-                          e.preventDefault();
-                          handleClick(e);
-                        }}
-                      >
-                        <MoreHorizIcon />
-                      </IconButton>
-                    )}
-                    <Menu
-                      id="basic-menu"
-                      anchorEl={anchorEl}
-                      open={open}
-                      onClose={handleClose}
-                      onClick={(e) => e.stopPropagation()}
-                      MenuListProps={{
-                        "aria-labelledby": "basic-button",
+        <Grid container flexWrap="nowrap">
+          <Grid item sx={{ paddingRight: "1rem" }}>
+            <Link to={`/profile/${post._id}`}>
+              <img src={profile.profileUrl} alt="lgoog" width="50px" />
+            </Link>
+          </Grid>
+          <Grid item flexGrow="1">
+            <Box>
+              <Grid
+                container
+                justifyContent="space-between"
+                alignItems="center"
+                flexWrap="nowrap"
+              >
+                <Grid item>
+                  <Box display="flex">
+                    <Typography
+                      sx={{ fontSize: "16px", fontWeight: 500, mr: "6px" }}
+                    >
+                      {profile.name}
+                    </Typography>
+                    <Typography
+                      sx={{ fontSize: "15px", mr: "6px", color: "#555" }}
+                    >
+                      @{profile.email}
+                    </Typography>
+                    <Typography
+                      sx={{ fontSize: "15px", mr: "6px", color: "#555" }}
+                    >
+                      .
+                    </Typography>
+                    <Typography
+                      sx={{ fontSize: "15px", mr: "6px", color: "#555" }}
+                    >
+                      {formatDistanceToNow(new Date(post.createdAt))}
+                    </Typography>
+                  </Box>
+                  <Box>
+                    <Typography sx={{ fontSize: "15px", color: "#555" }}>
+                      {post.text}
+                    </Typography>
+                  </Box>
+                </Grid>
+                <Grid item>
+                  {profile?._id === user?._id && (
+                    <IconButton
+                      aria-expanded={open ? "true" : undefined}
+                      onClick={(e) => {
+                        e.preventDefault();
+                        handleClick(e);
                       }}
                     >
-                      <MenuItem onClick={(e) => handleDeletePost(e)}>
-                        Delete Post
-                      </MenuItem>
-                    </Menu>
-                  </Grid>
+                      <MoreHorizIcon />
+                    </IconButton>
+                  )}
+                  <Menu
+                    id="basic-menu"
+                    anchorEl={anchorEl}
+                    open={open}
+                    onClose={handleClose}
+                    onClick={(e) => e.stopPropagation()}
+                    MenuListProps={{
+                      "aria-labelledby": "basic-button",
+                    }}
+                  >
+                    <MenuItem onClick={(e) => handleDeletePost(e)}>
+                      Delete Post
+                    </MenuItem>
+                  </Menu>
                 </Grid>
-                <Box
-                  display="flex"
-                  justifyContent="space-between"
-                  marginRight="5rem"
-                  marginTop=".8rem"
-                >
-                  <IconButton size="small">
-                    <SyncIcon fontSize="small" />
-                  </IconButton>
-                  <IconButton onClick={handleLike} size="small">
-                    {isLiked ? (
-                      <FavoriteIcon fontSize="small" />
-                    ) : (
-                      <FavoriteBorderIcon fontSize="small" />
-                    )} <small style={{ marginLeft: 5}}>{post.likes.length}</small>
-                  </IconButton>
-                  <IconButton size="small">
-                    <IosShareIcon fontSize="small" />
-                  </IconButton>
-                </Box>
+              </Grid>
+              <Box
+                display="flex"
+                justifyContent="space-between"
+                marginRight="5rem"
+                marginTop=".8rem"
+              >
+                <IconButton size="small">
+                  <SyncIcon fontSize="small" />
+                </IconButton>
+                <IconButton onClick={handleLike} size="small">
+                  {isLiked ? (
+                    <FavoriteIcon fontSize="small" />
+                  ) : (
+                    <FavoriteBorderIcon fontSize="small" />
+                  )}{" "}
+                  <small style={{ marginLeft: 5 }}>{post.likes.length}</small>
+                </IconButton>
+                <IconButton size="small">
+                  <IosShareIcon fontSize="small" />
+                </IconButton>
               </Box>
-            </Grid>
+            </Box>
           </Grid>
-        </Box>
-      </Link>
+        </Grid>
+      </Box>
     </>
   );
 }
